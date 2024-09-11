072ebd7ad0052cc5ee67c50e2660a55b51e81a8f
navya_mk <navya.krishnojirao@alten.com>
2024-09-11 16:27:30 +0530

    add comments

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import React, { useState } from 'react';
 import { Container, Grid, TextField, Button, Typography, Box, Paper } from '@mui/material';
 
 function App() {
+  
+  // State variables to hold blue token inputs
   const [blueTokenCount, setBlueTokenCount] = useState('');
   const [bluePrefix, setBluePrefix] = useState('');
   const [blueTokensPerRow, setBlueTokensPerRow] = useState('');
 
+  // State variables to hold red token inputs
   const [redTokenCount, setRedTokenCount] = useState('');
   const [redPrefix, setRedPrefix] = useState('');
   const [redTokensPerRow, setRedTokensPerRow] = useState('');
 
+  // State to store the generated tokens
   const [tokens, setTokens] = useState({ blue: [], red: [] });
 
+
+  // Function to generate tokens based on input values
   const handleGenerate = () => {
     const blueTokens = Array.from({ length: blueTokenCount }, (_, index) => `${bluePrefix}${index + 1}`);
     const redTokens = Array.from({ length: redTokenCount }, (_, index) => `${redPrefix}${index + 1}`);
+    
     setTokens({ blue: blueTokens, red: redTokens });
   };
 
+  // Function to clear all inputs and reset token state
   const handleClear = () => {
     setBlueTokenCount('');
     setBluePrefix('');
@@ -25,6 +33,7 @@ function App() {
     setRedTokenCount('');
     setRedPrefix('');
     setRedTokensPerRow('');
+
     setTokens({ blue: [], red: [] });
   };
 
@@ -33,9 +42,9 @@ function App() {
       <Paper elevation={3} style={{ padding: '2rem', marginTop: '2rem' }}>
         <Typography variant="h6"><b>Token Generator</b></Typography>
         <br />
+        
+        {/* Input fields for blue tokens */}
         <Grid container spacing={2}>
-
-          {/* Blue Token Inputs */}
           <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
             <TextField
               label="Number of Blue Tokens"
@@ -73,7 +82,7 @@ function App() {
         </Grid>
         <br />
 
-        {/* Red Token Inputs */}
+        {/* Input fields for red tokens */}
         <Grid container spacing={2}>
           <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
             <TextField
@@ -112,6 +121,7 @@ function App() {
         </Grid>
         <br />
 
+        {/* Buttons for generating and clearing tokens */}
         <Grid container spacing={2}>
           <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
           </Grid>
@@ -124,8 +134,9 @@ function App() {
         </Grid>
       </Paper>
 
-      {/* Token Display */}
+      {/* Token display section */}
       <Box mt={4}>
+        {/* Display blue tokens */}
         <Typography variant="h6">Blue Tokens:</Typography>
         <Grid container spacing={2}>
           {tokens.blue.map((token, index) => (
@@ -135,6 +146,7 @@ function App() {
           ))}
         </Grid>
 
+        {/* Display red tokens */}
         <Typography variant="h6" mt={4}>Red Tokens:</Typography>
         <Grid container spacing={2}>
           {tokens.red.map((token, index) => (
